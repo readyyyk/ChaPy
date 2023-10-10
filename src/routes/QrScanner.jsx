ee8a099006e179cafb7162d318fd168fbb197535
readyyyk <mr.sairexer@gmail.com>
2023-10-10 11:21:55 +0300

    [F] (closed #44) `Added:` button for camera permission in `/scanner` route

--- src/routes/QrScanner.jsx
+++ src/routes/QrScanner.jsx
@@ -1,8 +1,9 @@
 import React, {useEffect, useState} from 'react';
 import {useNavigate} from 'react-router-dom';
 
-import {Box, Button, Typography} from '@mui/material';
+import {Box, Button, Stack, Typography} from '@mui/material';
 import LaunchIcon from '@mui/icons-material/Launch';
+import CameraAltIcon from '@mui/icons-material/CameraAlt';
 
 import {useZxing} from 'react-zxing';
 
@@ -15,16 +16,34 @@ const QrScanner = () => {
     const [text, setText] = useState('No data found!');
     const [isAbleToConnect, setIsAbleToConnect] = useState('');
 
+    const callPermission = async () => {
+        try {
+            navigator.getUserMedia = navigator.getUserMedia ||
+                navigator.webkitGetUserMedia ||
+                navigator.mozGetUserMedia ||
+                navigator.msGetUserMedia;
+            await navigator.mediaDevices.getUserMedia({video: true});
+            location.reload();
+        } catch (e) {}
+    };
+
     const {ref} = useZxing({
         onDecodeResult(result) {
             setText(result.getText());
         },
     });
-
     useEffect(() => {
         setIsAbleToConnect(/\/[a-zA-Z]{5}$/.test(text) ?
             /\/[a-zA-Z]{5}$/.exec(text)[0] : false);
     }, [text]);
+
+    navigator.permissions.query({name: 'camera'}).then((res) => {
+        res.onchange = (e) => {
+            localStorage['_isCameraAllowed'] = e.state === 'granted';
+            location.reload();
+        };
+        localStorage['_isCameraAllowed'] = res.state==='granted';
+    });
 
     return (
         <Box
@@ -51,19 +70,35 @@ const QrScanner = () => {
                 height: '300px',
                 border: '1.33px solid #fff',
                 borderRadius: 4,
-            }}/>
+            }} />
 
-            <Typography>{text}</Typography>
-            <Button
-                onClick={()=>navigate(isAbleToConnect)}
-                variant='outlined'
-                size='large'
-                sx={{
-                    display: isAbleToConnect ? '' : 'none',
-                }}
-            >
-                Connect <LaunchIcon sx={{ml: .5}}/>
-            </Button>
+            <Stack useFlexGap gap={'2rem'}>
+                <div>
+                    <Typography>{text}</Typography>
+                    <Button
+                        onClick={()=>navigate(isAbleToConnect)}
+                        variant='outlined'
+                        size='large'
+                        sx={{
+                            display: isAbleToConnect ? '' : 'none',
+                        }}
+                    >
+                        Connect <LaunchIcon sx={{ml: .5}}/>
+                    </Button>
+                </div>
+                <Button
+                    onClick={()=>callPermission()}
+                    variant='outlined'
+                    color={'success'}
+                    size='large'
+                    sx={{
+                        display: localStorage['_isCameraAllowed']==='true' ?
+                            'none' : 'flex',
+                    }}
+                >
+                    <CameraAltIcon/>
+                </Button>
+            </Stack>
         </Box>
     );
 };
